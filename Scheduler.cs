8a3b473c6eddcca9103cf1107e05753fec4481b4
ram16 <thomass.montaine@gmail.com>
2024-11-20 19:08:07 +0100

    Update Scheduler.cs

--- Scheduler.cs
+++ Scheduler.cs
@@ -35,11 +35,11 @@ namespace _ARK_
             return schedulable;
         }
 
-        public Schedulable AddAction(in Action action, [CallerMemberName] string callerMember = null) => AddSchedulable(new Schedulable(callerMember) { action = action });
-        public Schedulable AddFunc(in Func<bool> moveNext, [CallerMemberName] string callerMember = null) => AddSchedulable(new Schedulable(callerMember) { moveNext = moveNext });
-        public Schedulable AddRoutine(in IEnumerator routine, [CallerMemberName] string callerMember = null) => AddSchedulable(new Schedulable(callerMember) { routine = routine });
-        public Schedulable AddTask(in Action task, [CallerMemberName] string callerMember = null) => AddSchedulable(new Schedulable(callerMember) { _task = task });
-        public Schedulable AddTask(in Task task, [CallerMemberName] string callerMember = null) => AddSchedulable(new Schedulable(callerMember) { task = task });
+        public Schedulable AddAction(in Action action, [CallerMemberName] string callerName = null) => AddSchedulable(new Schedulable(callerName) { action = action });
+        public Schedulable AddFunc(in Func<bool> moveNext, [CallerMemberName] string callerName = null) => AddSchedulable(new Schedulable(callerName) { moveNext = moveNext });
+        public Schedulable AddRoutine(in IEnumerator routine, [CallerMemberName] string callerName = null) => AddSchedulable(new Schedulable(callerName) { routine = routine });
+        public Schedulable AddTask(in Action task, [CallerMemberName] string callerName = null) => AddSchedulable(new Schedulable(callerName) { _task = task });
+        public Schedulable AddTask(in Task task, [CallerMemberName] string callerName = null) => AddSchedulable(new Schedulable(callerName) { task = task });
 
         public T AddISchedulable<T>(in T schedulable) where T : ISchedulable
         {
@@ -69,11 +69,15 @@ namespace _ARK_
 
     public class SequentialScheduler : Scheduler
     {
-        public Schedulable InsertAction(in Action action) => InsertISchedulable(new Schedulable() { action = action });
-        public Schedulable InsertFunc(in Func<bool> moveNext) => InsertISchedulable(new Schedulable() { moveNext = moveNext });
-        public Schedulable InsertRoutine(in IEnumerator routine) => InsertISchedulable(new Schedulable() { routine = routine });
-        public Schedulable InsertTask(in Action task) => InsertISchedulable(new Schedulable() { _task = task });
-        public Schedulable InsertTask(in Task task) => InsertISchedulable(new Schedulable() { task = task });
+        public readonly ThreadSafe<bool> isTick = new();
+
+        //----------------------------------------------------------------------------------------------------------
+
+        public Schedulable InsertAction(in Action action, [CallerMemberName] string callerName = null) => InsertISchedulable(new Schedulable(callerName) { action = action });
+        public Schedulable InsertFunc(in Func<bool> moveNext, [CallerMemberName] string callerName = null) => InsertISchedulable(new Schedulable(callerName) { moveNext = moveNext });
+        public Schedulable InsertRoutine(in IEnumerator routine, [CallerMemberName] string callerName = null) => InsertISchedulable(new Schedulable(callerName) { routine = routine });
+        public Schedulable InsertTask(in Action task, [CallerMemberName] string callerName = null) => InsertISchedulable(new Schedulable(callerName) { _task = task });
+        public Schedulable InsertTask(in Task task, [CallerMemberName] string callerName = null) => InsertISchedulable(new Schedulable(callerName) { task = task });
 
         public T InsertISchedulable<T>(in T schedulable) where T : ISchedulable
         {
@@ -91,6 +95,8 @@ namespace _ARK_
                 if (list.Count > 0)
                 {
                     ISchedulable schedulable = list[0];
+                    isTick.Value = true;
+
                     lock (schedulable)
                     {
                         if (!schedulable.Scheduled)
@@ -102,11 +108,10 @@ namespace _ARK_
                         schedulable.OnTick();
 
                         if (schedulable.Disposed)
-                        {
                             list.Remove(schedulable);
-                            Tick();
-                        }
                     }
+
+                    isTick.Value = false;
                 }
         }
     }
