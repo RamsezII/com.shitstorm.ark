0e60203349f088a8a135d067e404c5c779e8f4c9
ram16 <thomass.montaine@gmail.com>
2025-04-04 16:15:24 +0200

    vers nouveau CharacterSkip

--- NUCLEOR/Util_ark.cs
+++ NUCLEOR/Util_ark.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace _ARK_
 {
@@ -24,9 +25,13 @@ namespace _ARK_
             _ => '?',
         };
 
-        public static void SkipCharactersUntil(this string text, ref int read_i, in bool positive)
+        [System.Obsolete]
+        public static void SkipCharactersUntil(this string text, ref int read_i, in bool positive, in int increment = 1)
         {
-            while (read_i < text.Length)
+            if (string.IsNullOrWhiteSpace(text))
+                return;
+
+            while (read_i < text.Length && read_i >= 0)
             {
                 char c = text[read_i];
                 switch (c)
@@ -40,7 +45,7 @@ namespace _ARK_
                         break;
 
                     case '\\':
-                        ++read_i;
+                        read_i += increment;
                         break;
 
                     default:
@@ -50,14 +55,51 @@ namespace _ARK_
                             return;
                         break;
                 }
-                ++read_i;
+                read_i += increment;
             }
-            if (read_i > text.Length)
-                read_i = text.Length;
+            read_i = Mathf.Clamp(read_i, 0, text.Length);
+        }
+
+        public static void SkipCharactersUntil(this string text, ref int read_i, in bool positive, in string chars, in bool left_to_right)
+        {
+            HashSet<char> charSet = new(chars);
+            if (!left_to_right)
+                --read_i;
+            while (read_i < text.Length && read_i >= 0)
+            {
+                char c = text[read_i];
+                switch (c)
+                {
+                    case '\t':
+                    case '\r':
+                    case '\n':
+                        break;
+
+                    case '\\':
+                        if (left_to_right)
+                            ++read_i;
+                        else
+                            --read_i;
+                        break;
+
+                    default:
+                        if (positive == charSet.Contains(c))
+                            return;
+                        break;
+                }
+                if (left_to_right)
+                    ++read_i;
+                else
+                    --read_i;
+            }
+            read_i = Mathf.Clamp(read_i, 0, text.Length);
         }
 
         public static bool TryReadPipe(this string text, ref int read_i)
         {
+            if (string.IsNullOrWhiteSpace(text))
+                return false;
+
             SkipCharactersUntil(text, ref read_i, true);
             if (read_i < text.Length && text[read_i] == CHAR_PIPE)
             {
