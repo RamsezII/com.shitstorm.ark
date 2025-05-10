1cd690fccb645b4780ac97689c349968934e58d2
ram16 <thomass.montaine@gmail.com>
2025-05-11 01:01:48 +0200

    ne pas remember last_user

--- Runtime/NUCLEOR/MachineSettings.cs
+++ Runtime/NUCLEOR/MachineSettings.cs
@@ -7,15 +7,6 @@ namespace _ARK_
 {
     public static class MachineSettings
     {
-        [Serializable]
-        class Infos : JSon
-        {
-            static readonly string FILE_NAME = typeof(MachineSettings).TypeToFileName() + txt;
-            public static string GetFilePath() => Path.Combine(NUCLEOR.home_path.ForceDir().FullName, FILE_NAME);
-
-            public string last_user;
-        }
-
         public static DirectoryInfo GetUsersFolder() => Path.Combine(NUCLEOR.home_path, "Users").ForceDir();
         public static string GetPlayerFolder() => Path.Combine(GetUsersFolder().FullName, user_name.Value).ForceDir().FullName;
 
@@ -24,8 +15,6 @@ namespace _ARK_
         public static bool user_ready;
         static Action on_user_ready;
 
-        static Infos infos;
-
         public static DirectoryInfo[] users;
 
         //----------------------------------------------------------------------------------------------------------
@@ -33,36 +22,16 @@ namespace _ARK_
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         static void OnBeforeSceneLoad()
         {
-            infos = new();
             user_name.Reset();
             user_ready = false;
             on_user_ready = null;
             users = GetUsersFolder().GetDirectories();
-            TryReadUserName();
         }
 
         //----------------------------------------------------------------------------------------------------------
 
-        static void TryReadUserName()
-        {
-            string file_path = Infos.GetFilePath();
-
-            if (JSon.Read(ref infos, file_path, false, true))
-                if (!string.IsNullOrWhiteSpace(infos.last_user))
-                {
-                    Debug.Log($"[READ] {nameof(infos.last_user)}: \"{infos.last_user}\" ({file_path})".ToSubLog());
-                    user_name.Update(infos.last_user);
-                    OnUserReady();
-                }
-        }
-
         public static void SaveUserName(in string value)
         {
-            string file_path = Infos.GetFilePath();
-
-            infos.last_user = value;
-            infos.Save(file_path, true);
-
             user_name.Update(value);
             OnUserReady();
         }
