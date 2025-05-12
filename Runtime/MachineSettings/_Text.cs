f01707746e3fbe9a2f376072c974e2a3b60808ff
ram16 <thomass.montaine@gmail.com>
2025-05-13 01:20:31 +0200

    GIGA UPDATE

--- Runtime/MachineSettings/_Text.cs
+++ Runtime/MachineSettings/_Text.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.IO;
 
 namespace _ARK_
 {
     partial class MachineSettings
     {
         [Serializable]
-        public class Infos : JSon
+        public class Infos : ArkJSon
         {
-            static readonly string FILE_NAME = typeof(MachineSettings).FullName + json;
-            public static string GetFilePath() => Path.Combine(NUCLEOR.home_path.GetDir(true).FullName, FILE_NAME);
-
             public string last_user;
         }
 
@@ -20,13 +16,13 @@ namespace _ARK_
 
         static void ReadInfos()
         {
-            JSon.Read(ref infos, Infos.GetFilePath(), false, true);
+            ArkJSon.Read(ref infos, false, true);
         }
 
         static void SaveInfos()
         {
             infos.last_user = user_name.Value;
-            infos.Save(Infos.GetFilePath(), true);
+            infos.SaveArkJSon(true);
         }
     }
 }
---
+++ Runtime/Util/ArkJSon.cs
@@ -0,0 +1,47 @@
+﻿using _UTIL_;
+using System;
+using System.IO;
+
+namespace _ARK_
+{
+    public abstract class ArkJSon : JSon
+    {
+        public string GetFilePath() => Path.Combine(NUCLEOR.home_path.GetDir(true).FullName, GetType().FullName + json);
+        public string GetExtension() => "." + GetType().FullName + json;
+        public void SaveArkJSon(in bool log) => Save(GetFilePath(), log);
+        public static bool Read<T>(ref T json, in bool force, in bool log) where T : ArkJSon, new() => Read(ref json, json.GetFilePath(), force, log);
+    }
+
+
+    [Serializable]
+    public class ArkJSonArray<T> : ArkJSon where T : IBytes
+    {
+        public T[] array;
+
+        //----------------------------------------------------------------------------------------------------------
+
+        public override void WriteBytes(in BinaryWriter writer)
+        {
+            base.WriteBytes(writer);
+            if (array == null)
+                writer.Write((ushort)0);
+            else
+            {
+                writer.Write((ushort)array.Length);
+                for (int i = 0; i < array.Length; ++i)
+                    array[i].WriteBytes(writer);
+            }
+        }
+
+        public override void ReadBytes(in BinaryReader reader)
+        {
+            base.ReadBytes(reader);
+
+            ushort count = reader.ReadUInt16();
+            array = new T[count];
+
+            for (int i = 0; i < count; ++i)
+                array[i].ReadBytes(reader);
+        }
+    }
+}
