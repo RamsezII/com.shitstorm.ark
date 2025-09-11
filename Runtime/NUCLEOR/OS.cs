b82189a9ae4da9742f59a48a95e983b8e221c39d
ram16 <thomass.montaine@gmail.com>
2025-09-11 15:49:15 +0200

    Update OS.cs

--- Runtime/NUCLEOR/OS.cs
+++ Runtime/NUCLEOR/OS.cs
@@ -12,25 +12,24 @@ namespace _ARK_
 
         //--------------------------------------------------------------------------------------------------------------
 
+#if UNITY_EDITOR
+        [ContextMenu(nameof(SaveTexts))]
+        void SaveTexts() => OnSaveTexts();
+        protected virtual void OnSaveTexts()
+        {
+        }
+#endif
+
+        [ContextMenu(nameof(LoadTexts))]
+        void LoadTexts() => OnLoadTexts(true);
+        protected virtual void OnLoadTexts(in bool log)
+        {
+        }
+
         protected virtual void OnApplicationFocus(bool focus)
         {
             if (focus)
                 OnLoadTexts(false);
-        }
-
-#if UNITY_EDITOR
-        [ContextMenu(nameof(SaveTexts))]
-        void SaveTexts() => OnSaveTexts();
-
-        [ContextMenu(nameof(LoadTexts))]
-        void LoadTexts() => OnLoadTexts(true);
-#endif
-        protected virtual void OnSaveTexts()
-        {
-        }
-
-        protected virtual void OnLoadTexts(in bool log)
-        {
         }
 
         //--------------------------------------------------------------------------------------------------------------
