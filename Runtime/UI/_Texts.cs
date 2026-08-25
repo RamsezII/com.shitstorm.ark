fa5afdfa831075feaf992c8e7300b892bd105cb0
ram16 <thomass.montaine@gmail.com>
2026-08-25 20:56:23 +0200

    Update _Texts.cs

--- Runtime/UI/_Texts.cs
+++ Runtime/UI/_Texts.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using UnityEngine;
 
 namespace _ARK_
 {
@@ -15,29 +14,20 @@ namespace _ARK_
 
         public HSettings hsettings;
 
-        static int ScreenHeightFactor3D => Mathf.Max(1, Mathf.RoundToInt(Screen.height / 600f));
-        static int ScreenHeightFactor2D => Mathf.Max(1, Mathf.RoundToInt(Screen.height / 500f));
-
-        int
-            lastFactor3D = ScreenHeightFactor3D,
-            lastFactor2D = ScreenHeightFactor2D;
-
         //----------------------------------------------------------------------------------------------------------
 
         void IHomeTexts.OnSaveHTexts(in bool log)
         {
-            hsettings.ui3D_scale = ui3D_pixels_scaler.scaleFactor / lastFactor3D;
-            hsettings.ui2D_scale = ui2D.scaler.scaleFactor / lastFactor2D;
+            hsettings.ui3D_scale = ui3D_pixels_scaler.scaleFactor;
+            hsettings.ui2D_scale = ui2D.scaler.scaleFactor;
             hsettings.SaveStaticJSon(log);
         }
 
         void IHomeTexts.OnLoadHTexts(in bool log)
         {
-            lastFactor3D = ScreenHeightFactor3D;
-            lastFactor2D = ScreenHeightFactor2D;
             StaticJSon.ReadStaticJSon(out hsettings, true, log);
-            ui3D_pixels_scaler.scaleFactor = hsettings.ui3D_scale * lastFactor3D;
-            ui2D.scaler.scaleFactor = hsettings.ui2D_scale * lastFactor2D;
+            ui3D_pixels_scaler.scaleFactor = hsettings.ui3D_scale;
+            ui2D.scaler.scaleFactor = hsettings.ui2D_scale;
         }
     }
 }
