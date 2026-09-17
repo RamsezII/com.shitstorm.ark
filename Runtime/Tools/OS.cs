a08e07d16880fc8b45c385504c6366b1ebfc76a8
ram16 <thomass.montaine@gmail.com>
2026-09-17 03:44:31 +0200

    ﻿texts et layermasks etc

--- Runtime/Tools/OS.cs
+++ Runtime/Tools/OS.cs
@@ -1,6 +1,4 @@
-﻿using UnityEngine;
-
-namespace _ARK_
+﻿namespace _ARK_
 {
     public abstract class OS : ArkComponent2
     {
@@ -10,25 +8,8 @@ namespace _ARK_
         protected override void Awake()
         {
             DontDestroyOnLoad(gameObject);
+
             base.Awake();
-            OnLoadTexts(true);
-            NUCLEOR.delegates.OnApplicationFocus += () => OnLoadTexts(log: false);
-        }
-
-        //--------------------------------------------------------------------------------------------------------------
-
-#if UNITY_EDITOR
-        [ContextMenu(nameof(SaveTexts))]
-        void SaveTexts() => OnSaveTexts();
-        protected virtual void OnSaveTexts()
-        {
-        }
-#endif
-
-        [ContextMenu(nameof(LoadTexts))]
-        void LoadTexts() => OnLoadTexts(true);
-        protected virtual void OnLoadTexts(in bool log)
-        {
         }
     }
 }
